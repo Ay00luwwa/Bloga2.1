2bb20216b7dfb74d0ed6fc0a1a2d92d4aa57571d
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-01-04 23:35:58 +0100

    commit7

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!k)@2g!6vz(l%6twg4ppoc+-=o=4u&cmoxo=%io%p4zx-6prxk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['Ayooluwa904T.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['http://ayooluwa9040t.pythonanywhere.com/', '127.0.0.1', 'localhost']
 
 
 
