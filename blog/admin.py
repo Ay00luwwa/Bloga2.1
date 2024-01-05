f7cfc2f02091e1f803f4eb77d0b559d76138c3dd
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-01-05 01:02:02 +0100

    commit 12

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Post, BlogImage
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date_posted']
+
+@admin.register(BlogImage)
+class BlogImageAdmin(admin.ModelAdmin):
+    list_display = ['post', 'image']
+
+admin.site.register(Category)
