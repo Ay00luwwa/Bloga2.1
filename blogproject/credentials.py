fa6c614b06283b71ac9e236176960784f7ce5a55
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-01-14 19:51:41 +0100

    commit 20

--- blogproject/credentials.py
+++ blogproject/credentials.py
@@ -1,2 +1,2 @@
-MAIL = 'bloga.blog2.1@gmail.com'
-PASSWORD = 'rgpr fmcq fwbv etzv'
+# MAIL = 'bloga.blog2.1@gmail.com'
+# PASSWORD = 'rgpr fmcq fwbv etzv'
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from .credentials import MAIL, PASSWORD
+# from .credentials import MAIL, PASSWORD
 from pathlib import Path
 import os
 
@@ -167,8 +167,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = MAIL
-EMAIL_HOST_PASSWORD = PASSWORD
+EMAIL_HOST_USER = 'bloga.blog2.1@gmail.com'
+EMAIL_HOST_PASSWORD = 'vdyj xflp umab otks'
 DEFAULT_FROM_EMAIL = 'noreply@bloga.blog2.1@gmail.com'
 
 AUTHENTICATION_BACKENDS = [
